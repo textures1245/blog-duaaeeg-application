4827dd8c071a6559dccb32d6f2175d57290c63ae
Traiphakh stihhikaew <sirprak1245@gmail.com>
2024-05-17 11:27:34 +0700

    feat(auth-domain): Update user profile model and API gateway

--- src/lib/internal/model/auth/domains/user.ts
+++ src/lib/internal/model/auth/domains/user.ts
@@ -1,8 +1,9 @@
 import type { AxiosResponse } from 'axios';
 import type { DtoResponded } from '../..';
 import type { UserClaims } from './auth';
+import type { ProfileForm } from '../../../../../routes/user-profile/[id]/ProfileForm.svelte';
 
-type UserProfile = {
+export type UserProfile = {
 	first_name: string;
 	last_name: string;
 	bio: string;
@@ -15,14 +16,28 @@ export type User = {
 	uuid: string;
 	email: string;
 	user_profile: UserProfile;
+	subscriber: unknown[];
+	subscribing: unknown[];
 	created_at: string | Date;
 	updated_at: string | Date;
 };
 
 export interface TUserHandlerGateway {
 	getUser(req: DtoResponded<UserClaims>, token: string): Promise<AxiosResponse<User>>;
+	getUsers(token: string, fetchMode?: 'WITH_PASSWORD'): Promise<AxiosResponse<User[]>>;
+	updateUserProfile(
+		req: ProfileForm,
+		token: string,
+		userUuid: string
+	): Promise<AxiosResponse<UserProfile>>;
 }
 
 export interface TUserUsecase {
-	onGetUserData(token: string): Promise<DtoResponded<User>>;
+	onGetUserData(req: DtoResponded<UserClaims>, token: string): Promise<DtoResponded<User>>;
+	onUpdateUserProfile(
+		req: ProfileForm,
+		token: string,
+		userUuid: string
+	): Promise<DtoResponded<UserProfile>>;
+	onGetUsers(token: string, fetchMode?: 'WITH_PASSWORD'): Promise<DtoResponded<User[]>>;
 }
