1d1b46faac6cc196d4402aa4714eb7862d16de3a
Traiphakh stihhikaew <sirprak1245@gmail.com>
2024-05-17 19:15:38 +0700

    refactor(admin-users-route): Decrypt user passwords in load function

--- src/routes/admin/users/+page.server.ts
+++ src/routes/admin/users/+page.server.ts
@@ -5,27 +5,37 @@ import type { PageServerLoad } from './$types';
 import { Dto } from '$lib/internal/model';
 import type { AxiosError } from 'axios';
 import type { UserWithPwd } from './+page.svelte';
-
+import { AEWDecryption } from '$lib/internal/utils/encryption';
+import { AEW_KEY } from '$env/static/private';
 
 export const load: PageServerLoad = async ({ cookies }) => {
 	// get users data
 	try {
 		const { token } = CookiesJsonParser(cookies, 'token');
 		const res = await NewUserUsecase.onGetUsers(token.access_token, 'WITH_PASSWORD');
-		console.log(res.result);
+
 		if (res.status_code !== 200) {
 			console.error(res);
 			return fail(res.status_code, { message: res.message });
 		}
 
-		const users = res.result as UserWithPwd[];
+		let users = res.result as UserWithPwd[];
+		if (users.length > 0) {
+			users = users.map((user) => {
+				const keyBuffer = Buffer.alloc(32)
+                keyBuffer.write(AEW_KEY);
+				user.password = AEWDecryption(user.password, keyBuffer);
+				return user;
+			});
+		}
+		console.log(users);
 
 		return {
-        users
+			users
 		};
 	} catch (error) {
 		const res = Dto.ReturnError(`Failed to get users data, please try again.`, error as AxiosError);
-		console.error(res.message);
-		return fail(res.status_code, { message: res.message });
+		console.error(res);
+		return fail(res.status_code, { message: res });
 	}
 };
