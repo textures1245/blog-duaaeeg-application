cb4e73d2fd76a6d38c63735a226c1d80229a4709
Traiphakh stihhikaew <sirprak1245@gmail.com>
2024-05-17 11:26:27 +0700

    update(adapter): Add updateUserProfile method to APIUserGateway

--- src/lib/internal/adapters/handler/user/http/v1/gateway.ts
+++ src/lib/internal/adapters/handler/user/http/v1/gateway.ts
@@ -1,9 +1,18 @@
-import type { TUserHandlerGateway, User } from '$lib/internal/model/auth/domains/user';
+import type { TUserHandlerGateway, User, UserProfile } from '$lib/internal/model/auth/domains/user';
 import type { AxiosError, AxiosResponse } from 'axios';
 import { APIGateway } from '../../..';
 import type { UserClaims } from '$lib/internal/model/auth/domains/auth';
+import type { ProfileForm } from '../../../../../../../routes/user-profile/[id]/ProfileForm.svelte';
 import type { DtoResponded } from '$lib/internal/model';
 
+const header = (token: string, opt?: object) => {
+	return {
+		headers: {
+			Authorization: `Bearer ${token}`,
+			...opt
+		}
+	};
+};
 export default class APIUserGateway extends APIGateway implements TUserHandlerGateway {
 	constructor(baseURL?: string) {
 		super(baseURL);
@@ -11,13 +20,47 @@ export default class APIUserGateway extends APIGateway implements TUserHandlerGa
 
 	public async getUser(req: DtoResponded<UserClaims>, token: string): Promise<AxiosResponse<User>> {
 		try {
-			return this.client.get(`/${req.result.user_uuid}`, {
-				headers: {
-					Authorization: `Bearer ${token}`
-				}
-			});
+			return this.client.get(`/${req.result.user_uuid}`, header(token));
 		} catch (error: unknown) {
 			throw new Error(`Failed to get user data: ${(error as AxiosError).message}`);
+		}
+	}
+
+	public async updateUserProfile(
+		req: ProfileForm,
+		token: string,
+		userUuid: string
+	): Promise<AxiosResponse<UserProfile>> {
+		try {
+			const res = this.client.post(
+				`/${userUuid}/profile`,
+				req,
+				header(token, {
+					'Content-Type': 'multipart/form-data'
+				})
+			);
+			return res;
+		} catch (error: unknown) {
+			throw new Error(`Failed to update user profile: ${(error as AxiosError).message}`);
+		}
+	}
+
+	public async getUsers(
+		token: string,
+		fetchMode?: 'WITH_PASSWORD'
+	): Promise<AxiosResponse<User[]>> {
+		try {
+			if (fetchMode) {
+				return this.client.get(
+					'/',
+					header(token, {
+						'fetch-mode': fetchMode
+					})
+				);
+			}
+			return this.client.get('/', header(token));
+		} catch (error: unknown) {
+			throw new Error(`Failed to get users data: ${(error as AxiosError).message}`);
 		}
 	}
 }
