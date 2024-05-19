e76abff7b928d3ece7a6e21833536ae74a1cc6a0
Traiphakh stihhikaew <sirprak1245@gmail.com>
2024-05-19 14:33:03 +0700

    feat: Add toggle theme button to Navbar component

--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -5,6 +5,8 @@
 	import * as DropdownMenu from '$lib/components/ui/dropdown-menu';
 	import { Input } from '$lib/components/ui/input';
 	import * as Sheet from '$lib/components/ui/sheet/index';
+	import { toggleMode } from 'mode-watcher';
+	import { Moon, Sun } from 'svelte-radix';
 
 	export let menu: string[];
 	export let avatarSrc =
@@ -59,6 +61,15 @@
 				class="w-full rounded-lg bg-background pl-8 md:w-[200px] lg:w-[320px]"
 			/>
 		</div>
+		<Button on:click={toggleMode} variant="outline" size="icon">
+			<Sun
+				class="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"
+			/>
+			<Moon
+				class="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"
+			/>
+			<span class="sr-only">Toggle theme</span>
+		</Button>
 		{#if !user}
 			<DropdownMenu.Root>
 				<DropdownMenu.Trigger asChild let:builder>
---
+++ src/components/RouteInjection.svelte
@@ -0,0 +1,35 @@
+<script context="module" lang="ts">
+	import type { SvelteComponent } from "svelte";
+
+	export type VariantActive =
+		| 'link'
+		| 'default'
+		| 'destructive'
+		| 'outline'
+		| 'secondary'
+		| 'ghost';
+
+	export type RouteInjection = {
+		routeName: string;
+		hrefTarget: string;
+		classActive: VariantActive;
+		classDeactive: VariantActive;
+        icon: typeof SvelteComponent;
+	};
+
+	// export function shouldBeActive(elem: SvelteComponent, injection: RouteInjection) {
+	// 	const currentPath = window.location.pathname;
+	// 	if (injection.hrefTarget === currentPath) {
+	// 		return
+	// 	} else {
+	// 		elem.setAttribute('variant', injection.classDeactive);
+	// 	}
+	// }
+	export function shouldBeActive(injection: RouteInjection, currentPath: string): VariantActive {
+		if (injection.hrefTarget === currentPath) {
+			return injection.classActive;
+		} else {
+			return injection.classDeactive;
+		}
+	}
+</script>
